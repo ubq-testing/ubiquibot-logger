1ffe3d00060e77b9eee3a0258d8baba2e5991e77
Korrrba <gitcoiner@gmail.com>
2023-12-08 13:13:22 +0100

    feat: logging NPM module initial port from ubiquibot refactor/general
    Ported from
    https://github.com/pavlovcik/ubiquibot/commits/refactor/general commit 07ce845.

---
+++ dist/logs.d.ts
@@ -0,0 +1,74 @@
+import { SupabaseClient } from "@supabase/supabase-js";
+import { Context } from "probot";
+
+declare class PrettyLogs {
+  constructor();
+  fatal(message: string, metadata?: any): void;
+  error(message: string, metadata?: any): void;
+  ok(message: string, metadata?: any): void;
+  info(message: string, metadata?: any): void;
+  debug(message: string, metadata?: any): void;
+  verbose(message: string, metadata?: any): void;
+  private _logWithStack;
+  private _colorizeText;
+  private _formatStackTrace;
+  private _isEmpty;
+  private _log;
+}
+declare enum LogLevel {
+  FATAL = "fatal",
+  ERROR = "error",
+  INFO = "info",
+  VERBOSE = "verbose",
+  DEBUG = "debug",
+}
+
+declare class LogReturn {
+  logMessage: LogMessage;
+  metadata?: any;
+  constructor(logMessage: LogMessage, metadata?: any);
+}
+type FunctionPropertyNames<T> = {
+  [K in keyof T]: T[K] extends (...args: any[]) => any ? K : never;
+}[keyof T];
+type PublicMethods<T> = Exclude<FunctionPropertyNames<T>, "constructor" | keyof object>;
+type LogMessage = {
+  raw: string;
+  diff: string;
+  level: LogLevel;
+  type: PublicMethods<Logs>;
+};
+declare class Logs {
+  private _supabase;
+  private _context;
+  private _maxLevel;
+  private _queue;
+  private _concurrency;
+  private _retryDelay;
+  private _throttleCount;
+  private _retryLimit;
+  static console: PrettyLogs;
+  private _log;
+  private _addDiagnosticInformation;
+  ok(log: string, metadata?: any, postComment?: boolean): LogReturn | null;
+  info(log: string, metadata?: any, postComment?: boolean): LogReturn | null;
+  error(log: string, metadata?: any, postComment?: boolean): LogReturn | null;
+  debug(log: string, metadata?: any, postComment?: boolean): LogReturn | null;
+  fatal(log: string, metadata?: any, postComment?: boolean): LogReturn | null;
+  verbose(log: string, metadata?: any, postComment?: boolean): LogReturn | null;
+  constructor(supabase: SupabaseClient, retryLimit: number, logLevel: LogLevel, context: Context | null);
+  private _sendLogsToSupabase;
+  private _processLogs;
+  private _retryLog;
+  private _processLogQueue;
+  private _throttle;
+  private _addToQueue;
+  private _save;
+  static _commentMetaData(metadata: any, level: LogLevel): string;
+  private _diffColorCommentMessage;
+  private _postComment;
+  private _getNumericLevel;
+  static convertErrorsIntoObjects(obj: any): any;
+}
+
+export { type LogMessage, LogReturn, Logs };
