4d52aa6b152b496bf797e3839de1cad2f087aaad
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-06-26 14:18:51 +0100

    chore: pin yarn, update jest, ts, eslint

--- src/utils.ts
+++ src/utils.ts
@@ -2,32 +2,32 @@
 const ansiEscapeCodes = /\x1b\[\d+m|\s/g;
 
 function cleanLogs(
-    spy: jest.SpiedFunction<{
-        (...data: string[]): void;
-        (message?: string, ...optionalParams: string[]): void;
-    }>
+  spy: jest.SpiedFunction<{
+    (...data: string[]): void;
+    (message?: string, ...optionalParams: string[]): void;
+  }>
 ) {
-    const strs = spy.mock.calls.map((call) => call.map((str) => str?.toString()).join(" "));
-    return strs.flat().map((str) => cleanLogString(str));
+  const strs = spy.mock.calls.map((call) => call.map((str) => str?.toString()).join(" "));
+  return strs.flat().map((str) => cleanLogString(str));
 }
 
 export function cleanLogString(logString: string) {
-    return logString.replaceAll(ansiEscapeCodes, "").replaceAll(/\n/g, "").replaceAll(/\r/g, "").replaceAll(/\t/g, "").trim();
+  return logString.replaceAll(ansiEscapeCodes, "").replaceAll(/\n/g, "").replaceAll(/\r/g, "").replaceAll(/\t/g, "").trim();
 }
 
 export function cleanSpyLogs(
-    spy: jest.SpiedFunction<{
-        (...data: string[]): void;
-        (message?: string, ...optionalParams: string[]): void;
-    }>
+  spy: jest.SpiedFunction<{
+    (...data: string[]): void;
+    (message?: string, ...optionalParams: string[]): void;
+  }>
 ): string[] {
-    return cleanLogs(spy);
+  return cleanLogs(spy);
 }
 
 export function tryError() {
-    try {
-        throw new Error("This is an error");
-    } catch (e) {
-        return e as Error;
-    }
+  try {
+    throw new Error("This is an error");
+  } catch (e) {
+    return e as Error;
+  }
 }
